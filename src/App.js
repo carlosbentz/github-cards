bb5f26c323d71658aea7a9e7e1a018ce885b3eb9
Carlos Bentz <carlos.bentz@hotmail.com>
2020-11-11 17:35:30 -0300

    finished basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import "antd/dist/antd.css";
+import { Card } from "antd";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState();
+  const [active, setActive] = useState(false);
+
+  const handdleToggle = () => {
+    fetch("https://api.github.com/users/carlosbentz")
+      .then((res) => res.json())
+      .then((res) => {
+        setUser(res);
+      });
+    console.log(user);
+    console.log(active);
+    setTimeout(() => {
+      setActive(!active);
+    }, 500);
+  };
+
+  useEffect(() => {
+    console.log("teste");
+    console.log(user);
+    console.log(active);
+  }, [user]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handdleToggle}>Toggle User</button>
+      {active && (
+        <Card>
+          <h1>teste</h1>
+          <img src={user.avatar_url} alt="Error"></img>
+          <h2>{user.name}</h2>
+          <h3>{user.bio}</h3>
+          <h3>{user.location}</h3>
+        </Card>
+      )}
     </div>
   );
-}
+};
 
 export default App;
