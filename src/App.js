114e8a5b908da8b6fed4b1a9cbc4ada134acd263
Carlos Bentz <carlos.bentz@hotmail.com>
2020-11-12 14:23:47 -0300

    removed settimeout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import "antd/dist/antd.css";
 import { Card } from "antd";
 
+const { Meta } = Card;
+
 const App = () => {
   const [user, setUser] = useState();
   const [active, setActive] = useState(false);
@@ -12,32 +14,32 @@ const App = () => {
       .then((res) => res.json())
       .then((res) => {
         setUser(res);
+        setActive(!active);
       });
-    console.log(user);
-    console.log(active);
-    setTimeout(() => {
-      setActive(!active);
-    }, 500);
   };
-
-  useEffect(() => {
-    console.log("teste");
-    console.log(user);
-    console.log(active);
-  }, [user]);
 
   return (
     <div className="App">
-      <button onClick={handdleToggle}>Toggle User</button>
-      {active && (
-        <Card>
-          <h1>teste</h1>
-          <img src={user.avatar_url} alt="Error"></img>
-          <h2>{user.name}</h2>
-          <h3>{user.bio}</h3>
-          <h3>{user.location}</h3>
-        </Card>
-      )}
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <button onClick={handdleToggle} style={{ width: "25vw" }}>
+          Toggle User
+        </button>
+        {active && (
+          <Card
+            hoverable
+            style={{
+              width: "25vw",
+            }}
+            cover={<img alt="example" src={user.avatar_url} />}
+            title="Europe Street beat"
+            description="www.instagram.com"
+          >
+            <h2>{user.name}</h2>
+            <h3>{user.bio}</h3>
+            <h3>{user.location}</h3>
+          </Card>
+        )}
+      </div>
     </div>
   );
 };
